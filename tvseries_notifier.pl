190082e4728dd98e5846dc7632ae6ab2e65ce842
cristiparaschiv <cristianv.paraschiv@gmail.com>
2015-03-23 16:25:43 +0200

    update trakt.tv api

--- tvseries_notifier.pl
+++ tvseries_notifier.pl
@@ -17,6 +17,11 @@ my $api = "SECRET"; # Replace with your own API key
 my $ua = LWP::UserAgent->new;
 my $data;
 
+my $client_id = "151216db7d18bf7e6024552ce1548d795aba23f50fdd01a97c7fe6acf6f41ba2";
+$ua->default_header("Content-Type" => "application/json");
+$ua->default_header("trakt-api-version" => "2");
+$ua->default_header("trakt-api-key" => $client_id);
+
 my $date = localtime->strftime('%Y%m%d');
 my $length = '7';
 GetOptions (
@@ -38,7 +43,7 @@ my $subscription = {
 };
 
 # Get json from trakt.tv
-my $response = $ua->get('http://api.trakt.tv/calendar/shows.json/'.$api.'/'.$date.'/'.$length);
+my $response = $ua->get("https://api-v2launch.trakt.tv/calendars/shows/$date/$length?extended=images");
 if ($response->is_success) {
 	$data =  $response->decoded_content;
 } else {
@@ -51,26 +56,27 @@ my $r = $json->decode($data);
 # Process response
 my $message;
 my ($season, $number, $title, $aired, $banner);
-foreach my $days (@$r) {
-	$message .= "<h3>$days->{date}</h3>";
-	my $count = 0;
-	my $episodes = $days->{episodes};
-	foreach my $ep (@$episodes) {
-		my $key = lc($ep->{show}->{title});
-		if (exists $subscription->{$key}) {
-			$count = 1;
-			$season = $ep->{episode}->{season};
-			$number = $ep->{episode}->{number};
-			$title = $ep->{episode}->{title};
-			$aired = $ep->{episode}->{first_aired_iso};
-			$banner = $ep->{show}->{images}->{banner};
-			$message .= "&nbsp;&nbsp;<b>" . $ep->{show}->{title} . "</b><br>";
-			$message .= "&nbsp;&nbsp;<img src=\"" . $banner . "\" width=\"50%\"><br>";
-			$message .= "&nbsp;&nbsp;&nbsp;&nbsp;Season $season / episode $number: $title<br>";
-			$message .= "&nbsp;&nbsp;&nbsp;&nbsp;Airing time: $aired<br>";
-		}
-	}
-	if ($count == 0) {$message .= "&nbsp;&nbsp;-- no subscription for this date<br>";}
+
+foreach my $day (sort keys %$r) {
+    my $count = 0;
+    $message .= "<h3>$day</h3>";
+
+    foreach my $show (@{$r->{$day}}) {
+        my $key = lc($show->{show}->{title});
+        if (exists $subscription->{$key}) {
+            $count = 1;
+            $season = $show->{episode}->{season};
+            $number = $show->{episode}->{number};
+            $title = $show->{episode}->{title};
+            $aired = $show->{airs_at};
+            $banner = $show->{show}->{images}->{banner}->{full};
+            $message .= "&nbsp;&nbsp;<b>" . $show->{show}->{title} . "</b><br>";
+            $message .= "&nbsp;&nbsp;<img src=\"" . $banner . "\" width=\"50%\"><br>";
+            $message .= "&nbsp;&nbsp;&nbsp;&nbsp;Season $season / episode $number: $title<br>";
+            $message .= "&nbsp;&nbsp;&nbsp;&nbsp;Airing time: $aired<br>";
+        }
+    }
+    if ($count == 0) {$message .= "&nbsp;&nbsp;-- no subscription for this date<br>";}
 }
 
 my $email = Email::MIME->create_html(
